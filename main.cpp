3973e75f37f264c07e2ccb917924edfc8d79df96
JolokHD <jannes.althoff@gmail.com>
2019-11-03 08:18:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <iterator>
+#include <iomanip>
 #include <stdexcept>
 
 std::array<char, 64> Base64LookUpTable{ {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '/'} };
@@ -53,54 +54,82 @@ std::array<bool, 5> encodeLetterChar(char inchar){
     return intToBitArray<5>(dis);
 }
 int main(){
-    bool exit(false);
+    bool exit(true);
     while(exit){
-        //ENCODE
-        std::string in;
-        std::getline (std::cin,in);
-        int bitlen = in.length() * 5;
-        int addbits = 6 - (bitlen % 6);
-        std::vector<bool> chararr(addbits);
-        for(int i=0; i<(in.length()); i++){
-            std::array<bool, 5> temp = (encodeLetterChar(in.at(i)));
-            chararr.insert(chararr.begin()+(i*5), temp.begin(), temp.end());
+        std::cout << '>';
+        std::string cmdin;
+        std::getline (std::cin,cmdin);
+        std::vector<std::string> args;
+        std::istringstream iss(cmdin);
+        for(std::string s; iss >> s; ){
+            args.push_back(s);
         }
-        std::string returnstring;
-        std::array<bool, 6> arr;
-        for(int i=0; i<((bitlen+addbits)/6); i++){
-            std::copy(chararr.begin()+(i*6), chararr.begin()+((i+1)*6), arr.data());
-            returnstring += encodeBase64Char(arr);
-        }
-        if(addbits == 5){returnstring += '=';}
-        std::cout << returnstring;
-
-        //DECODE
-        bool removelast5Bits(false);
-        if (returnstring.back() == '='){
-            removelast5Bits = true;
-            returnstring.pop_back();
-        }
-        std::vector<bool> baseVec(returnstring.size()*6);
-        std::array<bool, 6> tempBase;
-        for(int i=0; i<returnstring.size(); i++){
-            tempBase = decodeBase64Char(returnstring.at(i));
-            std::copy(tempBase.begin(), tempBase.end(), baseVec.begin()+(i*6));
-        }
-        if(5-baseVec.size()%5 == 5){
-            if(baseVec.size() != 0){
-                if(removelast5Bits){
-                    baseVec.resize(baseVec.size()-5);
-                }
+        if(args.at(0) == "exit"){
+            exit = false;
+            return 0;
+        } else
+        if(args.at(0) == help){
+            std::cout << "exit: Exit this programm."                               << std::endl
+                      << "encode: Encodes a String"                                << std::endl
+                      << "decode: Decodes a encoded String"                        << std::endl
+                      << "You can only use Capital Letters for a String to encode" << std::endl;
+        } else
+        if(args.at(0) == "encode"){
+            std::cout << "Enter the String to encode: " << std::endl;
+            //ENCODE
+            std::string in;
+            std::getline (std::cin,in);
+            int bitlen = in.length() * 5;
+            int addbits = 6 - (bitlen % 6);
+            std::vector<bool> chararr(addbits);
+            for(int i=0; i<(in.length()); i++){
+                std::array<bool, 5> temp = (encodeLetterChar(in.at(i)));
+                chararr.insert(chararr.begin()+(i*5), temp.begin(), temp.end());
             }
+            std::string out;
+            std::array<bool, 6> arr;
+            for(int i=0; i<((bitlen+addbits)/6); i++){
+                std::copy(chararr.begin()+(i*6), chararr.begin()+((i+1)*6), arr.data());
+                out += encodeBase64Char(arr);
+            }
+            if(addbits == 5){out += '=';}
+            std::cout << "Encoded String: " << out << std::endl;
+        } else
+        if(args.at(0) == "decode"){
+            std::cout << "Enter the String to decode: " << std::endl;
+            std::string in;
+            std::getline (std::cin,in);
+            //DECODE
+            bool removelast5Bits(false);
+            if (in.back() == '='){
+                removelast5Bits = true;
+                in.pop_back();
+            }
+            std::vector<bool> baseVec(in.size()*6);
+            std::array<bool, 6> tempBase;
+            for(int i=0; i<in.size(); i++){
+                tempBase = decodeBase64Char(in.at(i));
+                std::copy(tempBase.begin(), tempBase.end(), baseVec.begin()+(i*6));
+            }
+            if(5-baseVec.size()%5 == 5){
+                if(baseVec.size() != 0){
+                    if(removelast5Bits){
+                        baseVec.resize(baseVec.size()-5);
+                    }
+                }
+            } else {
+                baseVec.resize(baseVec.size()-baseVec.size()%5);
+            }
+            std::string out;
+            for(int i=0; i<(baseVec.size()/5); i++){
+                std::array<bool, 5> temp;
+                std::copy(baseVec.begin()+(i*5), baseVec.begin()+((i+1)*5), temp.begin());
+                out += decodeLetterChar(temp);
+            }
+            std::cout << "Decoded String: " << out << std::endl;
         } else {
-            baseVec.resize(baseVec.size()-baseVec.size()%5);
+            std::cout << "Invalid Command"    << std::endl
+                      << "For help type help" << std::endl;
         }
-        std::string out;
-    for(int i=0; i<(baseVec.size()/5); i++){
-            std::array<bool, 5> temp;
-            std::copy(baseVec.begin()+(i*5), baseVec.begin()+((i+1)*5), temp.begin());
-            out += decodeLetterChar(temp);
-        }
-        std::cout << std::endl << out;
     }
 }
